454250b0d883c8b12d075316d3f3f4ba071dca99
Marcus Whybrow <marcus@marcuswhybrow.net>
2011-03-15 20:51:39 +0000

    Initial Commit

---
+++ g52cfj-icw2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  g52cfj-icw2
+//
+//  Created by Marcus Whybrow on 15/03/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
